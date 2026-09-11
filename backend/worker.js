8da22fd395bd3ec923ac1336b3b94b30f98edc57
Nipour Souq <nipour@local>
2026-09-11 21:53:08 +0300

    Fix Cloudflare Node import

--- backend/worker.js
+++ backend/worker.js
@@ -1,4 +1,4 @@
-const { httpServerHandler } = require("cloudflare:node");
+import { httpServerHandler } from "cloudflare:node";
 const http = require('http');
 const Busboy = require('busboy');
 const fs = require('fs');
